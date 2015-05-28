ec621ac209287642a439b05520c1576e1ce5bf44
jviding <jasu.viding@helsinki.fi>
2015-05-28 20:33:26 +0300

    Updated Gruntfile.js and package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -411,7 +411,8 @@ module.exports = function (grunt) {
     }
   });
 
-
+  grunt.registerTask('deploy', ['buildcontrol']);
+  
   grunt.registerTask('serve', 'Compile then start a connect web server', function (target) {
     if (target === 'dist') {
       return grunt.task.run(['build', 'connect:dist:keepalive']);
